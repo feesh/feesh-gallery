ecf88489449794f15b897543b5e47ac72ca2024a
Trish Ang <trish@counsyl.com>
2017-01-04 02:33:13 -0800

    Adding toggling dummy functions for lightbox

--- src/app.js
+++ src/app.js
@@ -9,10 +9,36 @@ class App extends React.Component {
   constructor() {
     super();
 
+    this.toggleLightbox = this.toggleLightbox.bind(this);
+
     this.state = {
+      open: false,
       selected_pic: null,
     };
   }
+
+  // Open lightbox if a photo is selected, otherwise close
+  toggleLightbox(pic) {
+    // What pic is currently open
+    const current_pic = this.state.selected_pic;
+
+    // Open the selected pic
+    this.setState({
+      open: true,
+      selected_pic: pic,
+    });
+  }
+
+  // For navigating to the next photo while the lightbox is open
+  toggleArrow(pic, dir) {
+    // Check to see which direction was clicked
+    // Set + or - based on direction
+    // Check current position in array
+      // If +/- 1 in that direction is more than 0 or count
+        // Then loop to the end or the beginning of the array
+      // Else update selected_pic with the new index
+  }
+
 
   render() {
     const { photos } = this.props;
@@ -22,9 +48,16 @@ class App extends React.Component {
         <div className="content">
           { this.state.selected_pic &&
             // Show lightbox if a pic is selected
-            <Lightbox photos={photos} />
+            <Lightbox
+              photos={photos}
+              selected_pic={this.state.selected_pic}
+              toggleArrow={(pic, dir) => this.toggleArrow(pic, dir)}
+            />
           }
-          <Thumbnails photos={photos} />
+          <Thumbnails
+            photos={photos}
+            toggleLightbox={(pic) => this.toggleLightbox(pic)}
+          />
         </div>
       </section>
     );
