e88d05dcff02d34331e2486d059d510705c8f834
Trish Ang <trish@counsyl.com>
2017-01-04 02:33:40 -0800

    Passing in selected pic

--- src/components/lightbox-section.js
+++ src/components/lightbox-section.js
@@ -3,9 +3,11 @@ import classNames from 'classnames';
 
 class Lightbox extends React.Component {
   render() {
+    const { photos, selected_pic } = this.props;
+
     return (
       <section className="content__lightbox">
-        <p>The lightbox</p>
+        <p>{selected_pic}</p>
       </section>
     );
   }
