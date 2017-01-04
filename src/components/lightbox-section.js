f630225f326bbb8d41d7e6a1d163ff806918869d
Trish Ang <trish@counsyl.com>
2017-01-04 03:30:33 -0800

    Emoji icon styles and close btn

--- src/components/lightbox-section.js
+++ src/components/lightbox-section.js
@@ -4,7 +4,7 @@ import classNames from 'classnames';
 class Lightbox extends React.Component {
   // Pass close signal to parent
   toggleLightbox(pic) {
-    console.log('Close lightbox bc overlay clicked');
+    console.log('Close lightbox bc overlay or x clicked');
     this.props.toggleLightbox(pic);
   }
 
@@ -19,27 +19,38 @@ class Lightbox extends React.Component {
     return (
       <section className="content__lightbox">
         <div className="modal">
-          <span
-            className="arrow prev"
-            onClick={() => this.toggleArrow(selected_pic, 'prev')}
-            title="View previous photo"
-          >
-            ◀️
-          ️</span>
-          <div className="image">
-            <img
-              src={photos[selected_pic].images.standard_resolution.url}
-              alt={photos[selected_pic].caption.text}
-            />
-            <h3>{photos[selected_pic].caption.text}</h3>
+          <div className="header">
+            <span
+              className="bigmoji close"
+              onClick={() => this.toggleLightbox(null)}
+              title="Close lightbox"
+            >
+              ❌
+            </span>
           </div>
-          <span
-            className="arrow next"
-            onClick={() => this.toggleArrow(selected_pic, 'next')}
-            title="View next photo"
-          >
-            ▶️
-          ️</span>
+          <div className="modal-body">
+            <span
+              className="bigmoji arrow prev"
+              onClick={() => this.toggleArrow(selected_pic, 'prev')}
+              title="View previous photo"
+            >
+              ◀️
+            ️</span>
+            <div className="image">
+              <img
+                src={photos[selected_pic].images.standard_resolution.url}
+                alt={photos[selected_pic].caption.text}
+              />
+              <h3>{photos[selected_pic].caption.text}</h3>
+            </div>
+            <span
+              className="bigmoji arrow next"
+              onClick={() => this.toggleArrow(selected_pic, 'next')}
+              title="View next photo"
+            >
+              ▶️
+            ️</span>
+          </div>
         </div>
         <div
           className="overlay"
